351959aa6e91e17a0eb616ff0c6acaa102913a53
LaupWing <loc_strict@hotmail.com>
2021-04-07 17:27:31 +0200

    8

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
+import {useState} from 'react'
+
 const Home = () => {
+   const [name, setName] = useState('Laup Wing')
+
    const handleClick = () =>{
-      console.log('handle on click')
+      setName('Hau Wing')
    }
 
    return (
       <div className="home">
          <h2>Home</h2>
+         <p>{name}</p>
          <button onClick={handleClick}>CLick me</button>
       </div>
    )
