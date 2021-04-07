3034c18f5042099eb58a92fdb9997c2676128cd1
LaupWing <loc_strict@hotmail.com>
2021-04-07 19:53:44 +0200

    10

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,25 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const Home = () => {
-   const [name, setName] = useState('Laup Wing')
+   const [blogs] = useState([
+      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+   ])
 
-   const handleClick = () =>{
+   const handleClick = () => {
       setName('Hau Wing')
    }
 
    return (
       <div className="home">
          <h2>Home</h2>
-         <p>{name}</p>
+         <p>{blogs.map(x=>(
+            <div key={x.id}>
+               <h2>{x.title}</h2>
+               <p>Written by {x.author}</p>
+            </div>
+         ))}</p>
          <button onClick={handleClick}>CLick me</button>
       </div>
    )
